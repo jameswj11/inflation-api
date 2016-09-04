86e7468a0b27c503798ed5450142ae7780ee4b7a
James Johnson <jameswj111@gmail.com>
2016-09-03 21:45:39 -0400

    fix api route

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@
 const router = require('express').Router()
 const cpiModel = require('../models/cpi')
 
-router.get('/cpi', cpiModel.calculate, (req, res)=>{
+router.get('/', cpiModel.calculate, (req, res)=>{
   res.json(res.inflationAdjusted)
 })
 
